96255e9ab31f6381be9a80c31218367159f22d1e
lucas <lucas.videla994@gmail.com>
2025-07-21 16:47:35 -0300

    chore: Bump version to 1.1.1 for install script fix

--- internal/core/version.go
+++ internal/core/version.go
@@ -2,4 +2,4 @@ package core
 
 // Version es la versión actual del CLI
 // Esta variable centralizada debe ser la única fuente de verdad para la versión
-var Version = "1.1.0"
+var Version = "1.1.1"
